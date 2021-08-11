0715191623812faeb3b76c5c85bf91304a4b3012
liwenjie <447047479@qq.com>
2021-08-11 16:09:18 +0800

    a

--- webpack/webpack.render.dev.js
+++ webpack/webpack.render.dev.js
@@ -1,18 +1,34 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpackMerge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    resolve: {
-        extensions: ['.js', '.jsx', 'ts', 'tsx'],
-        alias: {
-            '@src': path.join(__dirname, '../', 'app/render'),
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)/
-            }
-        ]
-    }
-}
+const devConfig = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, '../app/renderer/app.jsx'),
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  target: 'electron-renderer',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'),
+    compress: true,
+    host: '127.0.0.1', // webpack-dev-server启动时要指定ip，不能直接通过localhost启动，不指定会报错
+    port: 7001, // 启动端口为 7001 的服务
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 👇 以此文件为模版，自动生成 HTML
+      template: path.resolve(__dirname, '../app/renderer/index.html'),
+      filename: path.resolve(__dirname, '../dist/index.html'),
+      chunks: ['index'],
+    }),
+  ],
+};
+
+module.exports = webpackMerge.merge(baseConfig, devConfig);
